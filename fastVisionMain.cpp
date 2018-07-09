e087336833b62716623706a5a857192f217da9f0
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-07-09 06:54:59 +0200

    minor changes

--- fastVisionMain.cpp
+++ fastVisionMain.cpp
@@ -1,27 +1,29 @@
+// packages that need to be installed beforehand, must be called again during compilation
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
-// need to be installed beforehand
 
 int main()
 {
     char *outText;
 
+    // create new tesseract api object
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-    // Initialize tesseract-ocr with English
-    if (api->Init(NULL, "eng")) {
-        fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
-    }
 
-    // Open input image with leptonica library, no path needed as image is in base directory
-    // Here we can also explore converting pdfs to images
+    // open input image with leptonica library, no path needed as image is in base directory
+    // here we can also explore converting pdfs to images
     Pix *image = pixRead("test.png");
+
+    // initialize tesseract-ocr with English
+    api->Init(NULL, "eng");
     api->SetImage(image);
-    // Get OCR result
+    // specify bounding rectangle to reduce search space
+    api->SetRectangle(50, 28, 525, 120);
+
+    // get OCR result
     outText = api->GetUTF8Text();
     printf("OCR output:\n%s", outText);
 
-    // Destroy used object and release memory
+    // destroy used object and release memory
     api->End();
     delete [] outText;
     pixDestroy(&image);
